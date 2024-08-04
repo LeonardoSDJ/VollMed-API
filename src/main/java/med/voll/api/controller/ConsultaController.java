373c0fd3219bb6dbf0fc21f49cea318db64ed8e6
Leonardo Souza <leonardo.sdj97@gmail.com>
2024-08-04 17:23:45 -0300

    Inicio funcao agendar consulta

--- src/main/java/med/voll/api/controller/ConsultaController.java
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -1,10 +1,10 @@
 package med.voll.api.controller;
 
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
 import med.voll.api.domain.consulta.DadosDetalhamentoConsulta;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +16,9 @@ public class ConsultaController {
 
     @PostMapping
     @Transactional
-    public ResponseEntity agendar(@RequestBody @Valid DadosAgendamentoConsulta dados){
+    public ResponseEntity agendar(@RequestBody @Valid DadosAgendamentoConsulta dados) {
         System.out.println(dados);
         return ResponseEntity.ok(new DadosDetalhamentoConsulta(null, null, null, null));
     }
+
 }
--- src/main/java/med/voll/api/domain/consulta/DadosAgendamentoConsulta.java
+++ src/main/java/med/voll/api/domain/consulta/DadosAgendamentoConsulta.java
@@ -2,10 +2,19 @@ package med.voll.api.domain.consulta;
 
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Null;
+import med.voll.api.domain.medico.Especialidade;
 
 import java.time.LocalDateTime;
 
-public record DadosAgendamentoConsulta(Long idMedico, @NotNull Long idPaciente, @Null @Future LocalDateTime data) {
+public record DadosAgendamentoConsulta(
+        Long idMedico,
 
+        @NotNull
+        Long idPaciente,
+
+        @NotNull
+        @Future
+        LocalDateTime data,
+
+        Especialidade especialidade) {
 }

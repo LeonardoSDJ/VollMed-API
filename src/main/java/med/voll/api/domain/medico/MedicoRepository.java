ea060ed36aae80559d78e987aae89c4ef5a391fc
Leonardo <leonardo.sdj97@gmail.com>
2024-08-13 13:39:45 -0300

    Validacoes de agendamento de consulta

--- src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -26,4 +26,12 @@ order by rand()
 limit 1
             """)
     Medico escolherMedicoAleatorioLivreNaData(Especialidade especialidade, LocalDateTime data);
+
+    @Query("""
+            SELECT m.ativo
+            FROM Medico m
+            WHERE
+            m.id = :idMedico
+            """)
+    Boolean findAtivoById(Long id);
 }
